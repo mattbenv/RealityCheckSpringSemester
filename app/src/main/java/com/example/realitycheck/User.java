0866704e4fa97d6ce2819f6154cf2aa1d16f29b7
ThomasHearn <50595654+ThomasHearn@users.noreply.github.com>
2021-10-12 19:29:54 -0400

    added User class

---
+++ app/src/main/java/com/example/realitycheck/User.java
@@ -0,0 +1,17 @@
+package com.example.realitycheck;
+
+
+public class User {
+    public String email;
+    public String username;
+    //more information to track
+
+    public User(){
+
+    }
+
+    public User(String email, String username){
+        this.email=email;
+        this.username  = username;
+    }
+}
