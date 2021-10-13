caf753d4d8ae6a8d6c54ae7dc696a9ac1bed2f7f
Matthew Benvenuto <mattbenv@udel.edu>
2021-10-13 00:03:01 -0400

    UserClass Updates

--- app/src/main/java/com/example/realitycheck/SecondFragment.java
+++ app/src/main/java/com/example/realitycheck/SecondFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.realitycheck.databinding.FragmentSecondBinding;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -107,7 +108,7 @@ public class SecondFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            User user = new User(emailValue,usernameValue);
+                            User user = new User(emailValue,usernameValue,0, 0,0);
                             FirebaseDatabase.getInstance().getReference()
                                     .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                     .setValue(user);
--- app/src/main/java/com/example/realitycheck/User.java
+++ app/src/main/java/com/example/realitycheck/User.java
@@ -1,17 +1,25 @@
 package com.example.realitycheck;
 
 
+import java.util.ArrayList;
+
 public class User {
     public String email;
     public String username;
+    public int followers;
+    public int following;
+    public int friends;
+    //public ArrayList<User> followers;
+    //public ArrayList<User> following;
+    //public ArrayList<User> friends;
+
     //more information to track
 
-    public User(){
-
-    }
-
-    public User(String email, String username){
+    public User(String email, String username, int followers, int following, int friends ){
         this.email=email;
         this.username  = username;
+        this.followers = followers;
+        this.following = following;
+        this.friends = friends;
     }
 }
