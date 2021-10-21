d6e4df57171dac2d8bea0607fe1c739e39820e18
Matthew Benvenuto <mattbenv@udel.edu>
2021-10-21 15:57:13 -0400

    Algorithim for user sorting

--- app/src/main/java/com/example/realitycheck/User.java
+++ app/src/main/java/com/example/realitycheck/User.java
@@ -64,4 +64,79 @@ public class User {
         }
         return friends;
     }
+
+    public void sort( ArrayList<User> users){
+
+    }
+}
+
+class GFG
+{
+
+    public static void sortStrings(String[] arr, int n)
+    {
+        String storage;
+
+        // Sorting strings using bubble sort
+        for (int j = 0; j < n - 1; j++)
+        {
+            for (int i = j + 1; i < n; i++)
+            {
+                if (arr[j].compareTo(arr[i]) > 0)
+                {
+                    storage = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = storage;
+                }
+            }
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        String[] names = {
+                "mike",
+                "mike",
+                "thomas",
+                "jonathan",
+                "akon",
+                "drake",
+                "biggie",
+                "jayz",
+                "oneil",
+                "benvenuto",
+                "thommmy",
+                "aaron",
+                "macmiller",
+                "mAC",
+                "mac",
+                "miller",
+                "juicewrld",
+                "boogiewitahoodie",
+                "jcole",
+                "excision",
+                "porterrobinson",
+                "pporterRobinson",
+                "matt",
+                "thomas",
+                "michael",
+                "martin",
+                "deb",
+                "patrick",
+                "max",
+                "geroge",
+                "carol",
+                "cheikna",
+                "jonathan",
+                "deb",
+                "bennydabull"
+
+        };
+        int n = names.length;
+        sortStrings(names, n);
+        System.out.println("Sorted Names: ");
+        for (int i = 0; i < n; i++)
+            System.out.println("" + names[i]);
+    }
 }
