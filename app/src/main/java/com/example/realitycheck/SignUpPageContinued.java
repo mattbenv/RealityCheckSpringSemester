15980479dd1e5eb5f56d53609c6d9733b4b007d2
Matthew Benvenuto <mattbenv@udel.edu>
2021-11-03 15:22:20 -0400

    dependencies

--- app/src/main/java/com/example/realitycheck/SignUpPageContinued.java
+++ app/src/main/java/com/example/realitycheck/SignUpPageContinued.java
@@ -25,6 +25,7 @@ import com.example.realitycheck.databinding.SignupcontinuedBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
