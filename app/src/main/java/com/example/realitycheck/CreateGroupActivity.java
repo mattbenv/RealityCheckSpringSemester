b67a4984c0df7a4abcee9e51bb77fd7e43c683f4
pationeill <oneillp@udel.edu>
2021-12-07 19:49:19 -0500

    Group creation now includes picture selection. Still needs to be tested to see if the image path is being uploaded to Firebase.

--- app/src/main/java/com/example/realitycheck/CreateGroupActivity.java
+++ app/src/main/java/com/example/realitycheck/CreateGroupActivity.java
@@ -1,32 +1,47 @@
 package com.example.realitycheck;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.realitycheck.databinding.CreateGroupBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CreateGroupActivity extends Fragment {
 
+    private static final int PICK_IMAGE_REQUEST = 22;
     private FirebaseAuth mAuth;
     //public static EditText groupName, addMembers;
 
     public String groupName, bio, profileImagePath;
+    private Uri profileImage;
     public boolean privacy; //true is private, false is public
     public int size; //number of members
     public ArrayList<String> members = new ArrayList<>();
@@ -55,6 +70,21 @@ public class CreateGroupActivity extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //allows for selection of picture from device
+        binding.selectPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //this calls the method for selecting pics
+                selectImage();
+
+            }
+        });
+        //allows for upload of picture using camera
+        binding.takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takeImage();
+            }
+        }); //this calls the method for taking pics
         // this is to create a group
         binding.CreateGroupActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +92,7 @@ public class CreateGroupActivity extends Fragment {
                 groupName = binding.groupName.getText().toString().trim();
                 bio = binding.addBio.getText().toString().trim();
                 newGroupName = groupName;
+                uploadImage();
              //   members.add(binding.addMembers.getText().toString().trim()); // need to fix
                 //profileImagePath =;
                 privacy = binding.setSecurity.isChecked();
@@ -107,83 +138,88 @@ public class CreateGroupActivity extends Fragment {
 
     }
 
-    // this functions tests the validity of the credentials: including password length etc...
-    /*public boolean testInformation(){
-        //gets string values
-        String groupnameValue = groupname.getText().toString().trim();
-        String addmembersValue = addmembers.getText().toString().trim();
+    //todo: allow for users to use camera to take a picture
+    public void takeImage(){
 
-        //test format of information
-        if(groupnameValue.isEmpty()){
-            email.setError("Group Name is required");
-            email.requestFocus();
-            return false;
-        }
-        if(m.find()==false){
-            binding.email.setError("Invalid email address");
-            binding.email.requestFocus();
-            return false;
-        }
-        if(usernameValue.isEmpty()){
-            username.setError("Username is required");
-            username.requestFocus();
-            return false;
-        }
-        if(passwordValue.length()<6){
-            password.setError("Password must be at least 6 characters");
-            password.requestFocus();
-            return false;
-        }
-        if(confirmedpasswordValue.isEmpty()){
-            confirmpassword.setError("Please confirm your password");
-            confirmpassword.requestFocus();
-            return false;
+    }
 
-        }
 
-        if(confirmedpasswordValue.compareTo(passwordValue)!=0){
-            confirmpassword.setError("Passwords do not match");
-            confirmpassword.requestFocus();
-            return false;
-        }
-        else{
-            return true;
+
+
+
+    //allows user to select an image from their device
+    public void selectImage()
+    {
+        Intent intent = new Intent(); // intent = screen
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+    //this is called everytime when selectImage() is called
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            profileImage = filePath;
+            profileImagePath = filePath.toString();
+            try {
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(
+                                getContext().getContentResolver(),
+                                filePath);
+                binding.imageView.setImageBitmap(bitmap);            }
+
+            // when image selction fails
+            //todo: write an exception that is printed when user denies access to gallery
+            catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
         }
     }
 
-    public void registerGroup(){
-
-        //Authenticate and add user to database
-
-        mAuth.createUserWithEmailAndPassword(email.getText().toString().trim(),password.getText().toString().trim())
-                .addOnCompleteListener(SignUpPage.this.getActivity(),new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-
-                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                    .setDisplayName(binding.username.getText().toString())
-                                    .build();
-
-                            mAuth.getCurrentUser().updateProfile(profileUpdates);
-
-                            new AlertDialog.Builder(getActivity())
-                                    .setTitle("Account created successfully!")
-                                    .setMessage("Please continue building your profile")
-                                    .setCancelable(false)
-                                    .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            NavHostFragment.findNavController(SignUpPage.this)
-                                                    .navigate(R.id.action_SignUpPage_to_SignUpPageContinued);
-                                        }
-                                    }).show();
-
-
+    // this uploads image to the database
+    private void uploadImage() {
+        //creates a profile image path for the newly uploaded image
+        profileImagePath = groupName+ "_profile_picture" ;
+        final ProgressDialog progressDialog = new ProgressDialog(this.getContext());
+        progressDialog.setTitle("Uploading...");
+        if(profileImage!= null) {
+            progressDialog.show();
+            //adds image to the database
+            StorageReference ref = storageReference.child("images/"+ profileImagePath);
+            ref.putFile(profileImage)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(CreateGroupActivity.this.getActivity(), "Uploaded", Toast.LENGTH_SHORT).show();
                         }
-                    }
-
-                });
-
-    }*/
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(CreateGroupActivity.this.getActivity(), "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                        }
+                    });
+        }
+    }
 }
